448b807366c86e25fdf72d42650ae206280df70d
alu0100886870 <alu0100886870@ull.edu.es>
2019-12-04 15:39:25 +0000

    Primer commit con Git dentro de Unity

---
+++ Assets/Characters/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Animator animator;
+    private SpriteRenderer sp;
+    private Rigidbody2D rb;
+    public float speed = 5.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        sp = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+        
+
+        if (vertical > 0)
+        {
+            animator.SetInteger("Direction", 8);
+        }
+        else if (vertical < 0)
+        {
+            animator.SetInteger("Direction", 2);
+        }
+        else if (horizontal > 0)
+        {
+            animator.SetInteger("Direction", 6);
+            sp.flipX = true;
+        }
+        else if (horizontal < 0)
+        {
+            animator.SetInteger("Direction", 4);
+            sp.flipX = false;
+        }
+
+        if (vertical == 0 && horizontal == 0)
+            animator.enabled = false;
+        else
+            animator.enabled = true;
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        rb.AddForce(move * speed);
+       // transform.position += move * speed * Time.deltaTime;
+        
+
+    }
+}
---
+++ Assets/Characters/PushObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float fuerzaMovimiento = 30.0f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        GameObject otro = other.gameObject;
+        // Intentar ver si tiene un rigidbody2D
+        Rigidbody2D rb = otro.GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector2.right * fuerzaMovimiento) ;
+    }
+}
